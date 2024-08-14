437efc452b21398d7b25ca0f49b3a1d601800e61
tanisha0605 <tanishakanal@gmail.com>
2024-08-14 21:53:15 +0530

    Animations added

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,12 +1,28 @@
+'use client'
 import Image from "next/image";
 import bedImage from "../assets/images/emojistar.png"
 import sofaImage from "../assets/images/helix2.png"
+import { useScroll, useTransform,motion } from "framer-motion";
+import { useRef,useEffect } from "react";
+
 export const CallToAction = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const {scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start end','end end'],
+  })
+  useEffect(()=>{
+    scrollYProgress.on('change',(value )=> 
+      console.log("value",value))
+  },[]);
+
+  const translateY=useTransform(scrollYProgress,[0,1],[40,-40])
+
   return (
-    <div className="bg-black text-white py-[72px] sm:py-24 text-center">
+    <div className="bg-black text-white py-[72px] sm:py-24 text-center" ref={containerRef}>
       <div className="container max-w-xl relative">
-      <Image src={sofaImage} alt="Bed Image" className="absolute top-6 left-[calc(100%+36px)]"/>
-        <Image src={bedImage} alt="Bed Image" className="absolute -top-[120px] right-[calc(100%+24px)]"/>
+      <motion.div style={{translateY}}><Image src={sofaImage} alt="Bed Image" className="absolute top-6 left-[calc(100%+36px)]"/></motion.div>
+      <motion.div style={{translateY}}><Image src={bedImage} alt="Bed Image" className="absolute -top-[120px] right-[calc(100%+24px)]"/></motion.div>
         <h2 className="font-bold text-5xl tracking-tighter sm:text-6xl">Get instant access</h2>
         <p className="text-xl text-white/70 mt-5">
         Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.
--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,7 +1,23 @@
+'use client'
 import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
+import {motion,useScroll, useTransform} from "framer-motion";
+import { useEffect, useRef } from "react";
 
 export const ProductShowcase = () => {
+  const appImage = useRef<HTMLImageElement>(null);
+  const {scrollYProgress} = useScroll({
+    target:appImage,
+    offset:[
+      "start end","end end"
+    ]
+  });
+  useEffect(()=>{
+    scrollYProgress.on('change',(latestValue) => 
+      console.log("latestValue",latestValue))
+  },[]);
+  const rotateX = useTransform(scrollYProgress,[0,1],[15,0]);
+  const opacity = useTransform(scrollYProgress,[0,1],[.5,1]);
   return (
     <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px]">
       <div className="container ">
@@ -10,13 +26,22 @@ export const ProductShowcase = () => {
         <p className="text-center text-xl text-white/70 mt-5">Celebrate the joy of design with a website tailored to help architects create and refine their floorplans, turning visions into reality.</p>
         </div>
         <div className="flex items-center justify-center h-screen">
+          <motion.div
+          style={{
+            opacity:opacity,
+            rotateX:rotateX,
+            transformPerspective: "800px",
+          }}
+          >
           <Image 
           height={800} 
           width={800} 
           src={appScreen} 
           alt="The product screenshot" 
           className="mt-14 rounded-lg"
+          ref={appImage}
           />
+          </motion.div>
         </div>
 
       </div>
